6546e49320c5cae1ee90b5c8ca9195a1dfb89dd5
solitude-sweetened <danieliussmoriginas@gmail.com>
2025-10-31 01:10:17 +0200

    Library
    Added a my-library file

---
+++ mylib.h
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <numeric>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <sstream>
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::left;
+using std::right;
+using std::sort;
+using std::accumulate;
+using std::ifstream;
+using std::istringstream;
