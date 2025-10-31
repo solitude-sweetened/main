97a9e19bd21ae7473b5a212ed45a04db5f57fa72
solitude-sweetened <danieliussmoriginas@gmail.com>
2025-10-31 03:03:25 +0200

    Update to main.cpp based on task criteria
    Replacement of main.cpp file due to further review of task and requirements. The main file will now contain one loop for input and one for output,

--- main.cpp
+++ main.cpp
@@ -1,17 +1,26 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <iomanip>
-#include <algorithm>
+#include "mylib.h"
+#include "person.cpp"
 
-using namespace std;
+int main() {
+    srand(time(nullptr));
 
-class Person {
-private:
-    string name;
-    string surname;
-    vector<int> homework;
-    int exam;
-    double finalGrade;
+    int n;
+    cout << "Enter number of students: ";
+    cin >> n;
 
+    vector<Person> students(n);
+
+    for (int i = 0; i < n; ++i) {
+        cout << "\nStudent #" << i + 1 << ":\n";
+        students[i].inputData();
+        students[i].calculateFinalAverage();
+    }
+
+    cout << "\nName         Surname      Final(Avg)\n";
+    cout << "-------------------------------------\n";
+
+    for (auto &s : students)
+        s.printData();
+
+    return 0;
 }
