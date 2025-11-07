658b54ea5c35e5d2629800b32aa8910d3e7b03d6
solitude-sweetened <danieliussmoriginas@gmail.com>
2025-11-07 18:47:10 +0200

    input/output student header
    Header guard + function declarations

---
+++ io.h
@@ -0,0 +1,10 @@
+#ifndef IO_H
+#define IO_H
+
+#include "mylib.h"
+#include "person.h"
+
+void generateDataset(const std::string& filename, size_t count);
+void writeStudents(const std::vector<Person>& students, const std::string& filename);
+
+#endif
