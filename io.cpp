a9dacd4dd861940d00dca7eafa4357877245c43a
solitude-sweetened <danieliussmoriginas@gmail.com>
2025-11-07 18:58:17 +0200

    io source file
    Implementation file for generateDataset and writeStudents to create random student files and saves processed student results in a formatted table.

---
+++ io.cpp
@@ -0,0 +1,39 @@
+#include "io.h"
+
+void generateDataset(const std::string& filename, size_t count) {
+    std::ofstream out(filename);
+    if (!out) {
+        std::cerr << "Error: Cannot open file " << filename << " for writing.\n";
+        return;
+    }
+
+    out << "Name Surname HW1 HW2 HW3 HW4 HW5 Exam\n";
+
+    for (size_t i = 1; i <= count; ++i) {
+        out << "Name" << i << " Surname" << i << " ";
+        for (int j = 0; j < 5; ++j)
+            out << (1 + rand() % 10) << " ";
+        out << (1 + rand() % 10) << "\n";
+    }
+
+    out.close();
+}
+
+void writeStudents(const std::vector<Person>& students, const std::string& filename) {
+    std::ofstream out(filename);
+    if (!out) {
+        std::cerr << "Error: Cannot open file " << filename << " for writing.\n";
+        return;
+    }
+
+    out << std::left << std::setw(12) << "Name"
+        << std::setw(12) << "Surname"
+        << std::right << std::setw(12) << "Final(Avg)"
+        << " | " << std::setw(12) << "Final(Med)" << "\n";
+    out << std::string(12 + 12 + 12 + 3 + 12, '-') << "\n";
+
+    for (const auto& s : students)
+        out << s << "\n";
+
+    out.close();
+}
