4dda9b0d70bc6048f7e49714f8bdc54907c388d8
solitude-sweetened <danieliussmoriginas@gmail.com>
2025-11-20 04:33:35 +0200

    Uploading new version of io.h
    This version aligns with recent project changes

--- io.h
+++ io.h
@@ -4,10 +4,8 @@
 #include "mylib.h"
 #include "person.h"
 
-// Generates a dataset of random students (header + rows)
 void generateDataset(const std::string& filename, size_t count);
 
-// Write student results to file
 void writeStudents(const std::vector<Person>& students, const std::string& filename);
 
 std::ostream& operator<<(std::ostream& os, const Person& p);
