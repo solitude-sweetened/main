6814d0c2c688c79d50a7667f24fe1ff42f8872ef
solitude-sweetened <danieliussmoriginas@gmail.com>
2025-11-20 04:26:53 +0200

    Re-factored person.cpp again
    The program was not running due to issues with certain methods not being const, due to this, artificial intelligence was used to analyze and highlight the problem areas, as well as suggest fixes which were in turn implemented here.

--- person.cpp
+++ person.cpp
@@ -1,113 +1,93 @@
-#include "mylib.h"
+#include "person.h"
 
-class Person {
-
-  public:
-  string firstName, surname;
-  vector<int> homework;
-  int examResults;
-
-  double finalAverage;
-  double finalMedian;
-
-  string calcMethod;
-
-  // static counter
-  static inline int activeCount = 0;
-
-
-  double gradeAverage() {
+double Person::gradeAverage() const {
     if (homework.empty()) {
-      return 0.0;
+        return 0.0;
     }
 
     double sum = accumulate(homework.begin(), homework.end(), 0.0);
     return sum / static_cast<double>(homework.size());
-  }
+}
 
-  double averagePath() {
-    finalAverage = gradeAverage() * 0.4 + examResults * 0.6;
-    return finalAverage;
-  }
+double Person::averagePath() const {
+    return gradeAverage() * 0.4 + examResults * 0.6;
+}
 
-  double medianPath() {
-    finalMedian = gradeMedian() * 0.4 + examResults * 0.6;
-    return finalMedian;
-  }
+double Person::medianPath() const {
+    return gradeMedian() * 0.4 + examResults * 0.6;
+}
 
-  vector<int> RandHWGrade(int minimumGrades = 1, int maximumGrades = 10) {
-    vector<int> hwSet;
-
-    size_t count = (rand() % 10) + 1;
-
-    for (size_t i = 0; i < count; ++i) {
-      int value = minimumGrades + (rand() % (maximumGrades - minimumGrades + 1));
-      hwSet.push_back(value);
-    }
-
-    return hwSet;
-  }
-
-  int randExamResult(int minimumGrade = 1, int maximumGrade = 10) {
-    return minimumGrade + (rand() % (maximumGrade - minimumGrade + 1));
-  }
-
-  double gradeMedian() {
+double Person::gradeMedian() const {
     if (homework.empty()) {
-      return 0.0;
+        return 0.0;
     }
 
     size_t count = homework.size();
     size_t midpoint = count / 2;
 
     if (count % 2 == 0) {
-      double left = homework.at(midpoint - 1);
-      double right = homework.at(midpoint);
-      return (left + right) / 2.0;
+        double left = homework[midpoint - 1];
+        double right = homework[midpoint];
+        return (left + right) / 2.0;
     }
 
-    return homework.at(midpoint);
-  }
+    return homework[midpoint];
+}
 
-  Person()
-      : firstName(""),
-        surname(""),
-        homework(),
-        examResults(0),
-        finalAverage(0.0),
-        finalMedian(0.0),
-        calcMethod("")
-  {
-      activeCount++;
-  }
+vector<int> Person::RandHWGrade(int minimumGrades, int maximumGrades) {
+    vector<int> hwSet;
+    size_t count = (rand() % 10) + 1;
 
-  Person(const Person& other)
-      : firstName(other.firstName),
-        surname(other.surname),
-        homework(other.homework),
-        examResults(other.examResults),
-        finalAverage(other.finalAverage),
-        finalMedian(other.finalMedian),
-        calcMethod(other.calcMethod)
-  {
-      activeCount++;
-  }
+    for (size_t i = 0; i < count; ++i) {
+        int value = minimumGrades + (rand() % (maximumGrades - minimumGrades + 1));
+        hwSet.push_back(value);
+    }
 
-  Person& operator=(const Person& other) {
+    return hwSet;
+}
+
+int Person::randExamResult(int minimumGrade, int maximumGrade) {
+    return minimumGrade + (rand() % (maximumGrade - minimumGrade + 1));
+}
+
+Person::Person()
+    : firstName(""),
+      surname(""),
+      homework(),
+      examResults(0),
+      finalAverage(0.0),
+      finalMedian(0.0),
+      calcMethod("") 
+{
+    activeCount++;
+}
+
+Person::Person(const Person& other)
+    : firstName(other.firstName),
+      surname(other.surname),
+      homework(other.homework),
+      examResults(other.examResults),
+      finalAverage(other.finalAverage),
+      finalMedian(other.finalMedian),
+      calcMethod(other.calcMethod)
+{
+    activeCount++;
+}
+
+Person& Person::operator=(const Person& other) {
     if (this != &other) {
-      firstName = other.firstName;
-      surname = other.surname;
-      homework = other.homework;
-      examResults = other.examResults;
-      finalAverage = other.finalAverage;
-      finalMedian = other.finalMedian;
-      calcMethod = other.calcMethod;
+        firstName = other.firstName;
+        surname = other.surname;
+        homework = other.homework;
+        examResults = other.examResults;
+        finalAverage = other.finalAverage;
+        finalMedian = other.finalMedian;
+        calcMethod = other.calcMethod;
     }
     return *this;
-  }
+}
 
-  ~Person() {
-    activeCount--; // for future debugging purposes, if needed
+Person::~Person() {
+    activeCount--;
     cout << "Object removed. Active Person instances: " << activeCount << "\n";
-  }
-};
+}
