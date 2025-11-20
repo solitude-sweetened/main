c15390effe52d20c1f849faa57a512238fd5520e
solitude-sweetened <danieliussmoriginas@gmail.com>
2025-11-20 03:53:49 +0200

    Adding refactored version of main.cpp
    This version has been refactored from the previously interactive student-reading program into a fully automated program that generates large datasets, processes them, outputs the results, and reports performance metrics.

--- main.cpp
+++ main.cpp
@@ -1,62 +1,122 @@
 #include "mylib.h"
-#include "person.cpp"
+#include "person.h"
+#include "processing.h"
+#include "io.h"
+#include "timer.h"
+#include "exceptions.h"
 
-int main(){
-    string filename;
-    cout << "Enter the name of the student data file: ";
-    cin >> filename;
+int main() {
+    try {
 
-    ifstream studentFile(filename);
-    if (!studentFile) {
-        cerr << "Couldn't open the file: " << filename << endl;
+        // Dataset generation
+        cout << "Generating datasets. Please wait...\n";
+
+        generateDataset("dataset_10k.txt", 10000);
+        generateDataset("dataset_100k.txt", 100000);
+        generateDataset("dataset_1m.txt", 1000000);
+        generateDataset("dataset_10m.txt", 10000000);
+
+        cout << "Datasets generated.\n\n";
+
+        // List of datasets to process
+        vector<string> datasets = {
+            "dataset_10k.txt",
+            "dataset_100k.txt",
+            "dataset_1m.txt",
+            "dataset_10m.txt"
+        };
+
+        // Processing
+        for (const auto& file : datasets) {
+
+            cout << "Processing: " << file << "\n";
+
+            Timer timer;
+
+            // Reading
+            timer.start();
+
+            ifstream inp(file);
+            if (!inp) throw FileOpenError(file);
+
+            vector<Person> students;
+            string line;
+
+            getline(inp, line);
+            getline(inp, line);
+
+            while (getline(inp, line)) {
+                if (line.empty()) continue;
+                if (line[0] == '-') continue;
+
+                line.erase(remove(line.begin(), line.end(), '|'), line.end());
+
+                stringstream ss(line);
+                Person s;
+                int hw1, hw2, hw3, hw4, hw5, exam;
+
+                ss >> s.firstName >> s.surname >> hw1 >> hw2 >> hw3 >> hw4 >> hw5 >> exam;
+
+                if (ss.fail()) {
+                    throw ParseError("Invalid student line in file " + file + ": " + line);
+                }
+
+                s.homework = {hw1, hw2, hw3, hw4, hw5};
+                s.examResults = exam;
+
+                students.push_back(s);
+            }
+
+            inp.close();
+
+            double readTime = timer.elapsed();
+
+
+            // Sorting
+            timer.reset();
+
+            sort(students.begin(), students.end(), [](const Person& a, const Person& b) {
+                return a.firstName < b.firstName;
+            });
+
+            double sortTime = timer.elapsed();
+
+
+            // Splitting
+            timer.reset();
+
+            vector<Person> failed;
+            vector<Person> passed;
+
+            splitPassedFailed(students, failed, passed);
+
+            double splitTime = timer.elapsed();
+
+
+            // Writing passed
+            timer.reset();
+            writeStudents(passed, "passed_" + file);
+            double passedWriteTime = timer.elapsed();
+
+            // Writing failed
+            timer.reset();
+            writeStudents(failed, "failed_" + file);
+            double failedWriteTime = timer.elapsed();
+
+
+            // Summary output
+            cout << "Summary for " << file << "\n"
+                 << "Read time: " << readTime << " ms\n"
+                 << "Sort time: " << sortTime << " ms\n"
+                 << "Split time: " << splitTime << " ms\n"
+                 << "Write passed time: " << passedWriteTime << " ms\n"
+                 << "Write failed time: " << failedWriteTime << " ms\n"
+                 << "----------------------------------------------\n\n";
+        }
+
+    } catch (const std::exception& e) {
+        cerr << "Runtime error: " << e.what() << "\n";
         return 1;
-    }
-
-    vector<Person> students;
-
-    string line;
-    getline(studentFile, line); 
-    getline(studentFile, line); 
-
-    while (getline(studentFile, line)) {
-        if (line.empty()) continue;
-        if (line[0] == '-') continue;
-
-        line.erase(remove(line.begin(), line.end(), '|'), line.end());
-
-        stringstream ss(line);
-        Person s;
-        int hw1, hw2, hw3, hw4, hw5, exam;
-
-        ss >> s.firstName >> s.surname >> hw1 >> hw2 >> hw3 >> hw4 >> hw5 >> exam;
-        if (ss.fail()) continue; 
-
-        s.homework = {hw1, hw2, hw3, hw4, hw5};
-        s.examResults = exam;
-
-        students.push_back(s);
-    }
-
-    if (students.empty()) {
-        cerr << "No student data found in the list." << endl;
-        return 1;
-    }
-
-    sort(students.begin(), students.end(), [](const Person &a, const Person &b) {
-        return a.firstName < b.firstName;
-    });
-
-    cout << left << setw(15) << "Name"
-         << setw(15) << "Surname"
-         << setw(20) << "Final (Avg.)"
-         << setw(20) << "Final (Med.)" << endl;
-    cout << string(70, '-') << endl;
-
-    for (auto &s : students) {
-        cout << left << setw(15) << s.firstName
-             << setw(15) << s.surname
-             << setw(20) << fixed << setprecision(2) << s.averagePath()
-             << setw(20) << fixed << setprecision(2) << s.medianPath() << endl;
     }
 
     return 0;
